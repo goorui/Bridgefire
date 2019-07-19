6c619487f036173513c946aa7ae183c7c2f045de
guohaorui <hg1413@nyu.edu>
2019-07-19 22:58:51 +0800

    Finish data cleaning

--- Main.py
+++ Main.py
@@ -1,24 +1,23 @@
-import quandl, datetime, pandas
+import quandl, datetime
+import pandas as pd
 import numpy as np
 
 quandl.save_key('J3AKfEcoxgiV2TtMr2ue')
 quandl.read_key()
 
 # repeatedly get all data and convert to pandas data frame
-data_list = []
+df = pd.DataFrame([])
 cursor_id = None
-while True:
-    data = quandl.Datatable('SHARADAR/SF1').data(params = {'qopts':{'columns':['ticker', 'dimension', 'calendardate', 'netinc', 'marketcap', 'assets','liabilities', 'netinc', 'rnd', 'revenue']}})
+for i in range(2): # change to while True in working environment
+    data = quandl.Datatable('SHARADAR/SF1').data(params = {'qopts':{'columns':['ticker', 'dimension', 'calendardate', 'netinc', 'marketcap', 'assets','liabilities', 'netinc', 'rnd', 'revenue'], 'cursor_id':cursor_id}})
     cursor_id = data.meta['next_cursor_id']
-    data_list.append(data)
+    df = df.append(data.to_pandas())
     if cursor_id is None:
         break
-df = data_list.to_pandas()
-print(df.shape)
 
 # we only need annual data
 df = df[df['dimension'] == 'MRY']
-print(df.shape)
+print df.shape
 
 # sort the frame according to ticker and calendar date and data cleaning
 df = df.sort_values(by=['ticker', 'calendardate'])
@@ -27,24 +26,32 @@ df = df.sort_values(by=['ticker', 'calendardate'])
 df = df.drop_duplicates(keep = 'last') # if there's duplicate statements, we only keep the latest one
     # after dropping duplicates, there're about 8748 rows
 
-df['rnd'] = df['rnd'].replace(0,1) # because we want to use logarithm afterwards
-df['rnd'] = df['rnd'].fillna(1)
-df = df.drop(df[df.ticker == 'ZAAP'].index) # we drop ticker ZAAP since it has negative Research & Development
+df['rnd'] = df['rnd'].where(df['rnd'] > 0,1) # because we want to use logarithm afterwards, we replace all non-positive value with 1
 
-
-# create new columns in the frame following the tutorial, except for log_NC
+# create new columns in the frame following the tutorial, we replace log_NC with NC
+df = df.assign(NC = df['assets'] - df['liabilities'])
 df = df.assign(LEV = df['assets'] / df['liabilities'])
 df = df.assign(log_mcap = np.log(df['marketcap']))
+df = df.assign(log_mcap = np.log(df['marketcap']))
+
+df['netinc'] = np.abs(df['netinc'])
+df['netinc'] = df['netinc'].where(df['netinc'] > 0,1)
 df = df.assign(NI_p = np.log(np.abs(df['netinc'])))
+
 df = df.assign(NI_n = df['netinc'] + 1)
 df = df.assign(NI_n = np.log(np.abs(df['NI_n'][df['NI_n']<0])))
 df = df.assign(log_RD = np.log(df['rnd']))
+
+# calculate increase in revenue
 df = df.assign(g = np.nan)
-
 gb = df.groupby('ticker')
+df = pd.DataFrame([]); # create a new data frame to accommodate the new column g
+for group in gb:
+    length = len(group[1].index)
+    for i in range(1,length):
+        group[1].at[group[1].index[i], 'g'] = group[1].at[group[1].index[i], 'revenue'] - group[1].at[group[1].index[i-1], 'revenue']
+    df = df.append(group[1])
 
-count = 0;
-# for name, group in gb:
-#     print(group)
-#     count += 1
-#     print(count)
+# only keep desired columns
+df = df.drop(columns = ['dimension', 'marketcap', 'netinc', 'assets', 'liabilities', 'rnd', 'revenue'])
+print df.columns
